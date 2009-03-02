64ca69bb516cbe6f23fc50aabdd7f0454e992c73
Devin Austin <devin@devin-laptop.(none)>
2009-03-02 16:27:56 -0700

    Moved KiokuDB model config into otban.conf

--- lib/OTBAN/Model/KiokuDB.pm
+++ lib/OTBAN/Model/KiokuDB.pm
@@ -4,6 +4,6 @@ use Moose;
 BEGIN { extends qw(Catalyst::Model::KiokuDB) }
 
 # this is probably best put in the catalyst config file instead:
-__PACKAGE__->config( dsn => "dbi:SQLite:otban.sqlite" );
+#__PACKAGE__->config( dsn => "dbi:SQLite:otban.sqlite" );
 
 1;
