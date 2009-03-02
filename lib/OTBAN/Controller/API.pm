3ca1640b895ba6e3148006331c624fa553ef0b0c
Devin Austin <devin@devin-laptop.(none)>
2009-03-01 23:41:08 -0700

    Added a few skeleton REST methods for threads, and stub tests.

--- lib/OTBAN/Controller/API.pm
+++ lib/OTBAN/Controller/API.pm
@@ -21,6 +21,8 @@ display accordingly.
 
 =head2 index
 
+Just return some documentation here? i dunno.
+
 =cut
 
 sub index :Path :Args(0) {
@@ -28,6 +30,87 @@ sub index :Path :Args(0) {
 
     $c->response->body('Matched OTBAN::Controller::API in API.');
 }
+
+
+
+=head2 thread
+
+set up rest stuff for a thread
+
+=cut
+
+sub thread : Local : ActionClass('REST') { }
+
+=head2 thread_POST
+
+create a thread. defaults to a parent thread.
+
+=cut
+
+
+sub thread_POST {
+    my ($self, $c) = @_;
+    
+    ## insert code goes here
+
+    return $self->status_created(
+        $c,
+        location => ,        ## return location of the new thread
+        entity => {
+            post_success =>, ## determine whether the thread was posted successfully
+                             ## or not
+        }
+    );
+    
+}
+
+
+=head2 thread_GET
+
+get a thread's information.
+this gets a given thread in its entirety, with children.
+
+
+=cut
+
+sub thread_GET {
+    my ($self, $c) = @_;
+    
+    ## get code goes here
+    
+    return $self->status_ok(
+        $c, 
+        entity => {
+            thread =>, ## return our thread 
+        },    
+    
+    );
+
+
+}
+
+=head2 thread_PUT
+=cut
+
+sub thread_PUT{}
+
+=head2 thread_DELETE
+
+delete a thread and it's children.
+
+=cut
+
+sub thread_DELETE {
+    my ($self, $c) = @_;
+    
+    ## delete code goes here
+    
+    return $self->status_ok(
+        $c, 
+        entity => {
+            thread_delete_ok =>, ## deteremine if thread was deleted
+        }
+    );
 
 
 =head1 AUTHOR
--- t/controller_API.t
+++ t/controller_API.t
@@ -1,10 +1,32 @@
 use strict;
 use warnings;
-use Test::More tests => 3;
+use Test::More tests => 5;
 
 BEGIN { use_ok 'Catalyst::Test', 'OTBAN' }
 BEGIN { use_ok 'OTBAN::Controller::API' }
 
-ok( request('/api')->is_success, 'Request should succeed' );
 
+## let's make sure our URIs are okay
+ok( request('/api')->is_success, 'Request to root API should succeed' );
+    
+## test our URIs via REST requests ##
+
+## GET: thread
+ok( 
+    request( 
+        HTTP::Request->new( 
+            GET => '/api/thread') 
+        )->is_success, 
+        'Request to thread URI should succeed'
+);
+
+## POST: thread
+## i dunno if this is even the right usage. will look into it more.
+ok( 
+    request( 
+        HTTP::Request->new( 
+            POST => '/api/thread') 
+        )->is_success, 
+        'Request to thread URI should succeed'
+);
 
